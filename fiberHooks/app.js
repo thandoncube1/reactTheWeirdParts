5f53f593389a0734fdd5722698e344ce2b58b693
thandon263 <ncubethando20@gmail.com>
2024-03-15 10:44:27 -0400

    Fiber hooks react.

---
+++ fiberHooks/app.js
@@ -0,0 +1,10 @@
+// Hooks are directly connected to Fiber Nodes.
+// Hook is almost like attaching something to the branch of a tree,
+// React has Hooks as something that is hanging off the Nodes.
+// A hook is a javascript object -- A hook has its own state and a Queue
+/**
+ * It has a State and a Queue and a hook of its own. Each Fiber Node can have one or more hooks. Hooks are attached to Nodes of a tree. A linkedList is a type of data structure that links one Node to another. (Head to Tail).
+ */
+// You can build a queue using a LinkedList
+
+
