306b488c6af4feeb21bf3b747148a14bad834bfa
thandon263 <ncubethando20@gmail.com>
2024-05-30 12:02:41 -0400

    Formatting the document.

--- hooksState/app.js
+++ hooksState/app.js
@@ -4,32 +4,52 @@ let counterName = "One";
 root.render(<App />);
 
 function App() {
-    return (
-        <>
-            <h1>Counters</h1>
-            <section>
-               <Counter name="One" />
-               <Counter name="Two" />
-            </section>
-        </>
-    );
+  return (
+    <>
+      <h1>Counters</h1>
+      <section>
+        <Counter name="One" />
+        <Counter name="Two" />
+      </section>
+    </>
+  );
 }
 
 function Counter(props) {
-    const [numOfClicks, setNumOfClicks] = React.useState(0);
+  const [numOfClicks, setNumOfClicks] = React.useState(0);
 
-    return (
-        <article>
-            <h2>Counter {props.name}</h2>
-            <p>You clicked {numOfClicks} times</p>
-            <ButtonComponent buttonName={'INCREMENT'} dispatch={setNumOfClicks} numOfClicksOpr={numOfClicks + 1} />
-            <ButtonComponent buttonName={'DECREMENT'} dispatch={setNumOfClicks} numOfClicksOpr={numOfClicks - 1} disabledAttr={numOfClicks <= 0 ? "disabled" : false} />
-        </article>
-    );
+  return (
+    <article>
+      <h2>Counter {props.name}</h2>
+      <p>You clicked {numOfClicks} times</p>
+      <ButtonComponent
+        buttonName={"INCREMENT"}
+        dispatch={setNumOfClicks}
+        numOfClicksOpr={numOfClicks + 1}
+      />
+      <ButtonComponent
+        buttonName={"DECREMENT"}
+        dispatch={setNumOfClicks}
+        numOfClicksOpr={numOfClicks - 1}
+        disabledAttr={numOfClicks <= 0 ? "disabled" : false}
+      />
+    </article>
+  );
 }
 
-function ButtonComponent({ buttonName, dispatch, numOfClicksOpr, disabledAttr }) {
-    return <button className="button" onClick={() => dispatch(numOfClicksOpr)} disabled={disabledAttr}>
-        { buttonName }
+function ButtonComponent({
+  buttonName,
+  dispatch,
+  numOfClicksOpr,
+  disabledAttr,
+}) {
+  return (
+    <button
+      className="button"
+      onClick={() => dispatch(numOfClicksOpr)}
+      disabled={disabledAttr}
+    >
+      {buttonName}
     </button>
+  );
 }
